b992e1f6f2de279520fd9e6b437727048a22de06
MarcelIwanicki <90027751+MarcelSzkola@users.noreply.github.com>
2022-01-10 16:53:38 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+ for (int i=12; i<=42; i++) {
+  if(i%2==0) {
+   cout<<(i);
+   cout<<("\n");
+   }
+}
+ cout<<endl;
+ system("PAUSE");
+ return EXIT_SUCCESS;
+}
---
+++ z27.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
+ for ( int i=0;i<=20;i++)
+{ 
+ printf("* \n");
+}
+ cout<<endl;
+ system("PAUSE");
+ return EXIT_SUCCESS;
+}
